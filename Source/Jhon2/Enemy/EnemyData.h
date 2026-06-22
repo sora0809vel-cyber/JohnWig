23327bb3cbee5ae444219ddd4fcc7154bf3e0373
881PC-\student <nagashima@jec.ac.jp>
2026-06-22 11:32:42 +0900

    敵のパラメーターの箱追加

---
+++ Source/Jhon2/Enemy/EnemyData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Class.h"
+#include "EnemyData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JHON2_API UEnemyData : public UStruct
+{
+	GENERATED_BODY()
+	
+};
